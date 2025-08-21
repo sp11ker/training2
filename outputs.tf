148994411f2aea00e71cabb63d416052bcf3a50a
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-08-21 10:04:59 +0100

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.example.private_key_pem
+  filename        = "${path.module}/my-keypair.pem"
+  file_permission = "0400"
+}
