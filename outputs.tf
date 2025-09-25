723df3592f8f40228c3854dfa6cb8200687717f3
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-09-25 13:55:07 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,17 @@
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+output "dev_crm_public_ip" {
+  value = aws_instance.dev_crm.public_ip
+}
+
+output "dev_db_public_ip" {
+  value = aws_instance.dev_db.public_ip
+}
+
+output "prod_crm_public_ip" {
+  value = aws_instance.prod_crm.public_ip
+}
+
+output "prod_db_public_ip" {
+  value = aws_instance.prod_db.public_ip
 }
 
 output "private_key_pem" {
@@ -8,7 +20,7 @@ output "private_key_pem" {
 }
 
 resource "local_file" "private_key" {
-  content  = tls_private_key.example.private_key_pem
-  filename = "${path.module}/my-keypair.pem"
+  content         = tls_private_key.example.private_key_pem
+  filename        = "${path.module}/my-keypair.pem"
   file_permission = "0400"
 }
