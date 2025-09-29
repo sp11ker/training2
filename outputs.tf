d953f50c5d9d9e55f0536ef06105d124eab3c8ef
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-09-29 14:55:07 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,31 @@
-output "dev_crm_public_ip" {
-  value = aws_instance.dev_crm.public_ip
+# DEV Outputs
+output "dev_web_public_ip" {
+  value = aws_instance.web-dev.public_ip
+}
+
+output "dev_proc_public_ip" {
+  value = aws_instance.proc-dev.public_ip
 }
 
 output "dev_db_public_ip" {
-  value = aws_instance.dev_db.public_ip
+  value = aws_instance.db-dev.public_ip
 }
 
-output "prod_crm_public_ip" {
-  value = aws_instance.prod_crm.public_ip
+# PROD Outputs
+output "prod_web_public_ip" {
+  value = aws_instance.web-prod.public_ip
+}
+
+output "prod_proc_public_ip" {
+  value = aws_instance.proc-prod.public_ip
 }
 
 output "prod_db_public_ip" {
-  value = aws_instance.prod_db.public_ip
+  value = aws_instance.db-prod.public_ip
 }
 
+# Private key output (sensitive)
 output "private_key_pem" {
   value     = tls_private_key.example.private_key_pem
   sensitive = true
-}
-
-resource "local_file" "private_key" {
-  content         = tls_private_key.example.private_key_pem
-  filename        = "${path.module}/my-keypair.pem"
-  file_permission = "0400"
 }
