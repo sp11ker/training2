ed1987fe948bd9326ade83bc09f933a45c492070
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-09-29 15:29:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -238,7 +238,11 @@ resource "aws_instance" "db-prod" {
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.prod.id 
   key_name               = aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [aws_security_group.prod_ssh.id]   
+  vpc_security_group_ids = [aws_security_group.prod_ssh.id]  
+  tags = {
+    Name = "db"
+    Role = "db"
+    App  = "finance"
   }
 }
 
