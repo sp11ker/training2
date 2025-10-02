261db4cd900fd096ac2a48994e9b84c7938f5512
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-10-02 16:19:26 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,43 @@
+###############################
 # DEV Outputs
-output "dev_web_public_ip" {
-  value = aws_instance.web-dev.public_ip
+###############################
+
+output "dev_instance_1_public_ip" {
+  value = aws_instance.dev_instance_1.public_ip
 }
 
-output "dev_proc_public_ip" {
-  value = aws_instance.proc-dev.public_ip
+output "dev_instance_2_public_ip" {
+  value = aws_instance.dev_instance_2.public_ip
 }
 
-output "dev_db_public_ip" {
-  value = aws_instance.db-dev.public_ip
-}
-
+###############################
 # PROD Outputs
-output "prod_web_public_ip" {
-  value = aws_instance.web-prod.public_ip
+###############################
+
+output "prod_instance_1_public_ip" {
+  value = aws_instance.prod_instance_1.public_ip
 }
 
-output "prod_proc_public_ip" {
-  value = aws_instance.proc-prod.public_ip
+output "prod_instance_2_public_ip" {
+  value = aws_instance.prod_instance_2.public_ip
 }
 
-output "prod_db_public_ip" {
-  value = aws_instance.db-prod.public_ip
+###############################
+# IT Outputs
+###############################
+
+output "it_instance_1_public_ip" {
+  value = aws_instance.it_instance_1.public_ip
 }
 
-# Private key output (sensitive)
+output "it_instance_2_public_ip" {
+  value = aws_instance.it_instance_2.public_ip
+}
+
+###############################
+# Private Key Output (Sensitive)
+###############################
+
 output "private_key_pem" {
   value     = tls_private_key.example.private_key_pem
   sensitive = true
