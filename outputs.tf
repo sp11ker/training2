77211c7ef56f5ff869906385f369a487f3d27402
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-10-02 18:02:20 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,56 +1,17 @@
 ###############################
-# EC2 Instance Public IPs (By Name)
+# EC2 Instance Public IPs (Dynamically by Name)
 ###############################
 
 output "ec2_instances_public_ips" {
   value = {
-    # IT Instances
-    local.ec2_tags["it"]["it_instance_1"].Name   => aws_instance.it_instance_1.public_ip,
-    local.ec2_tags["it"]["it_instance_2"].Name   => aws_instance.it_instance_2.public_ip,
-
-    # DEV Instances
-    local.ec2_tags["dev"]["dev_instance_1"].Name => aws_instance.dev_instance_1.public_ip,
-    local.ec2_tags["dev"]["dev_instance_2"].Name => aws_instance.dev_instance_2.public_ip,
-
-    # PROD Instances
-    local.ec2_tags["prod"]["prod_instance_1"].Name => aws_instance.prod_instance_1.public_ip,
-    local.ec2_tags["prod"]["prod_instance_2"].Name => aws_instance.prod_instance_2.public_ip,
+    for instance in [
+      aws_instance.it_instance_1,
+      aws_instance.it_instance_2,
+      aws_instance.dev_instance_1,
+      aws_instance.dev_instance_2,
+      aws_instance.prod_instance_1,
+      aws_instance.prod_instance_2
+    ] :
+    instance.tags["Name"] => instance.public_ip
   }
-}
-
-###############################
-# Individual Public IP Outputs (Optional)
-###############################
-
-output "it_instance_1_public_ip" {
-  value = aws_instance.it_instance_1.public_ip
-}
-
-output "it_instance_2_public_ip" {
-  value = aws_instance.it_instance_2.public_ip
-}
-
-output "dev_instance_1_public_ip" {
-  value = aws_instance.dev_instance_1.public_ip
-}
-
-output "dev_instance_2_public_ip" {
-  value = aws_instance.dev_instance_2.public_ip
-}
-
-output "prod_instance_1_public_ip" {
-  value = aws_instance.prod_instance_1.public_ip
-}
-
-output "prod_instance_2_public_ip" {
-  value = aws_instance.prod_instance_2.public_ip
-}
-
-###############################
-# Private Key Output (Sensitive)
-###############################
-
-output "private_key_pem" {
-  value     = tls_private_key.example.private_key_pem
-  sensitive = true
 }
