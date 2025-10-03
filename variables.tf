1df18f3f824655146bb989a432e0cef4d8322b77
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-10-03 16:26:49 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
+# now bulit into main
 
-variable "ami_id" {
-  default = "ami-0e95a5e2743ec9ec9"  # Amazon Linux 2 in us-east-1
-}
-
+#variable "aws_region" {
+#  default = "us-east-1"
+#}
+#
+#variable "ami_id" {
+#  default = "ami-0e95a5e2743ec9ec9"  # Amazon Linux 2 in us-east-1
+#}
+#
 # the eu-north-1 ami = default = "ami-09dd028259b833925"  # Amazon Linux 2 in eu-north-1
